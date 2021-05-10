45b4326ccfee0071271c88eb8924da9d0ff7ca15
ProklUng <funtik21975@gmail.com>
2021-05-10 23:07:22 +0300

    Конфигуратор для Битрикса

---
+++ Services/TwigConfiguratorNative.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Prokl\TwigExtensionsPackBundle\Services;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBag;
+
+/**
+ * Class TwigConfiguratorNative
+ * @package Prokl\TwigExtensionsPackBundle\Services
+ */
+class TwigConfiguratorNative
+{
+    /**
+     * @var ContainerBag $containerBag Параметры.
+     */
+    private $containerBag;
+
+    /**
+     * @var array $config Конфигурация.
+     */
+    private $config = [];
+
+    /**
+     * TwigConfiguratorNative constructor.
+     *
+     * @param ContainerBag $containerBag Параметры.
+     */
+    public function __construct(ContainerBag $containerBag)
+    {
+        $this->containerBag = $containerBag;
+        if ($this->containerBag->has('twig_config')) {
+            $this->config = (array)$this->containerBag->has('twig_config');
+        }
+    }
+
+    /**
+     * @return array
+     */
+    public function paths() : array
+    {
+        return (array)$this->config['paths'];
+    }
+
+    /**
+     * @return array
+     */
+    public function config() : array
+    {
+        return (array)$this->config;
+    }
+}
