8555fb5fffe83de92bc435fbdfdf5cb3fccc2fa2
ProklUng <funtik21975@gmail.com>
2021-05-10 14:01:31 +0300

    csrf_token

---
+++ Twig/Extensions/SymfonyExtensions.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Prokl\TwigExtensionsPackBundle\Twig\Extensions;
+
+use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+/**
+ * Class SymfonyExtensions
+ * @package Prokl\TwigExtensionsPackBundle\Twig\Extensions
+ *
+ * @since 10.05.2021
+ */
+class SymfonyExtensions extends AbstractExtension
+{
+    /**
+     * @var CsrfTokenManagerInterface $csrfManager
+     */
+    private $csrfManager;
+
+    /**
+     * SymfonyExtensions constructor.
+     *
+     * @param CsrfTokenManagerInterface $csrfManager
+     */
+    public function __construct(CsrfTokenManagerInterface $csrfManager)
+    {
+        $this->csrfManager = $csrfManager;
+    }
+
+    /**
+     * Return extension name.
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'twig/symfony-extensions';
+    }
+
+    /**
+     * Функции.
+     *
+     * @return TwigFunction[]
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('csrf_token', [$this->csrfManager, 'getToken']),
+        ];
+    }
+}
