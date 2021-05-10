35ace8193b1411b182967e753324efd51a92e134
ProklUng <funtik21975@gmail.com>
2021-05-10 18:38:08 +0300

    Проброс globals

---
+++ Services/GlobalsTwig.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Prokl\TwigExtensionsPackBundle\Services;
+
+use Exception;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Twig\Environment;
+
+/**
+ * Class GlobalsTwig
+ * @package Prokl\TwigExtensionsPackBundle\Services
+ */
+class GlobalsTwig
+{
+    use ContainerAwareTrait;
+
+    /**
+     * @var array $globals
+     */
+    private $globals;
+
+    /**
+     * GlobalsTwig constructor.
+     * @param array $globals
+     */
+    public function __construct(array $globals)
+    {
+        $this->globals = $globals;
+    }
+
+    /**
+     * Инициализация событий Wordpress.
+     *
+     * @return void
+     */
+    public function hooksInit(): void
+    {
+        // Globals
+        add_filter('timber/twig', [$this, 'addGlobals']);
+
+        // Модификация глобального контекста Twig.
+        add_filter('timber_context', [$this, 'initTimberVariables']);
+    }
+
+    /**
+     * Globals.
+     *
+     * @param Environment $twig Twig.
+     *
+     * @return Environment
+     * @throws Exception
+     */
+    public function addGlobals(Environment $twig): Environment
+    {
+        if ($this->container->hasParameter('twig.globals')) {
+            foreach ($this->container->getParameter('twig.globals') as $name => $global) {
+                $twig->addGlobal($name, $global);
+            }
+        }
+
+        return $twig;
+    }
+
+    /**
+     * @param array $context
+     *
+     * @return array
+     * @throws Exception
+     */
+    public function initTimberVariables(array $context): array
+    {
+        return array_merge($context, $this->globals);
+    }
+}
